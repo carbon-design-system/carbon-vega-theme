3f82edfba57e92461b12832f78d5fdca4e5d5c87
Eliad Moosavi <theiliad@users.noreply.github.com>
2024-05-28 14:49:04 -0400

    update bundle path

--- scripts/sass-build.sh
+++ scripts/sass-build.sh
@@ -6,14 +6,14 @@ set -e
 rm -rf dist
 
 echo "building styles"
-sass ./src/scss/white.scss ./dist/white.css
-sass ./src/scss/white.scss ./dist/white.min.css --style=compressed
+sass ./src/scss/white.scss ./dist/css/white.css
+sass ./src/scss/white.scss ./dist/css/white.min.css --style=compressed
 
-sass ./src/scss/g10.scss ./dist/g10.css
-sass ./src/scss/g10.scss ./dist/g10.min.css --style=compressed
+sass ./src/scss/g10.scss ./dist/css/g10.css
+sass ./src/scss/g10.scss ./dist/css/g10.min.css --style=compressed
 
-sass ./src/scss/g90.scss ./dist/g90.css
-sass ./src/scss/g90.scss ./dist/g90.min.css --style=compressed
+sass ./src/scss/g90.scss ./dist/css/g90.css
+sass ./src/scss/g90.scss ./dist/css/g90.min.css --style=compressed
 
-sass ./src/scss/g100.scss ./dist/g100.css
-sass ./src/scss/g100.scss ./dist/g100.min.css --style=compressed
+sass ./src/scss/g100.scss ./dist/css/g100.css
+sass ./src/scss/g100.scss ./dist/css/g100.min.css --style=compressed
